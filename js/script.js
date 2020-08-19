c2237553f376fdd125e10364d28f2b2ebe5079bf
adprm <aditiyaprmn00@gmail.com>
2020-08-19 13:02:08 +0700

    modified data ajax

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ function showAllMenu() {
             $('#list-menu').append(`
             <div class="col-md-4 card-group">
                 <div class="card m-2">
-                    <img src="assets/images/`+ data.gambar + `" class="card-img-top">
+                    <img src="assets/images/`+ data.image + `" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">`+ data.name + `</h5>
-                        <p class="card-text">`+ data.deskripsi + `</p>
+                        <p class="card-text">`+ data.description + `</p>
                     </div>
                     <div class="card-footer">
                         <a href="#" class="btn btn-primary">Pesan Sekarang</a>
